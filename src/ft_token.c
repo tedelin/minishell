f18d72042fb63ba5ecf0978f0a865e6b9b6a0057
Tanguy EDELIN <tanguyed@proton.me>
2023-03-03 11:33:42 +0100

    fix segfault of ft_type function

--- src/ft_token.c
+++ src/ft_token.c
@@ -6,7 +6,7 @@
 /*   By: tedelin <tedelin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 16:10:45 by tedelin           #+#    #+#             */
-/*   Updated: 2023/03/03 11:28:21 by tedelin          ###   ########.fr       */
+/*   Updated: 2023/03/03 11:32:38 by tedelin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ void	ft_type(t_token **token)
 			cur->next->type = LIM;
 		else if (cur->next && cur->type != WORD)
 			cur->next->type = FD;
-		if (cur->next->type != WORD)
+		if (cur->next && cur->next->type != WORD)
 			cur = cur->next;
 		cur = cur->next;
 	}
@@ -44,7 +44,7 @@ int	main(void)
 	char *s;
 	t_token *token;
 
-	s = "< in ls -la | cat > out";
+	s = "< in 'ls -la | cat > out";
 	token = make_token(s);
 	ft_type(&token);
 	if (ft_parser(&token) == 1)
