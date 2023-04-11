8b60361b612246b036b9f19b4c423f3aa99eb404
catal mathis <42ecolepro@gmail.com>
2023-04-11 14:15:34 +0200

    echo

--- src/builtins/ft_echo.c
+++ src/builtins/ft_echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tedelin <tedelin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mcatal-d <mcatal-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 16:41:46 by tedelin           #+#    #+#             */
-/*   Updated: 2023/04/11 13:17:05 by tedelin          ###   ########.fr       */
+/*   Updated: 2023/04/11 13:44:25 by mcatal-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,19 @@ int	new_line(t_token **arg, int n)
 {
 	int		i;
 
-	i = 1;
-	while ((*arg)->value[i] && (*arg)->value[i] == 'n')
-		i++;
-	if (!(*arg)->value[i] && n++)
-		*arg = (*arg)->next;
+	while (*arg)
+	{
+		i = 1;
+		while ((*arg)->value[i] && (*arg)->value[i] == 'n')
+			i++;
+		if (!(*arg)->value[i] && i > 1)
+		{
+			n++;
+			(*arg) = (*arg)->next;
+		}
+		else
+			break;
+	}
 	return (n);
 }
 
@@ -40,12 +48,10 @@ void	ft_echo(t_cmd *cmd)
 		n = new_line(&arg, n);
 	while (arg)
 	{
-		if (ft_strncmp(arg->value, "-n", 2) || (!ft_strncmp(arg->value, "-n", 2)
-				&& arg->value[2]))
-			ft_putstr_fd(arg->value, 1);
+		ft_putstr_fd(arg->value, 1);
 		arg = arg->next;
-		// if (arg)
-		// 	ft_putstr_fd(" ", 1);
+		if (arg)
+			ft_putstr_fd(" ", 1);
 	}
 	if (n == 0)
 		ft_putstr_fd("\n", 1);
